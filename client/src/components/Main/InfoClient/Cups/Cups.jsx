d3c2f48515d7608a1b6fe944f3789767d448c463
alicia3194 <aliciacortinez@gmail.com>
2024-01-13 13:56:09 +0100

    cups logic and cups form

--- client/src/components/Main/InfoClient/Cups/Cups.jsx
+++ client/src/components/Main/InfoClient/Cups/Cups.jsx
@@ -1,10 +1,30 @@
-import React from "react";
-import FormCups from "./FormCups"
+import React, { useState } from "react";
+import FormCups from "./FormCups";
+import { useInfoCliente } from "../../../../context/InfoClienteProvider"; 
+import { Link } from "react-router-dom";
 
 const Cups = () => {
-  return <><FormCups/>
-  <a href="#">No tengo el CUPS</a>
-  </>;
+  const { setCup } = useInfoCliente();
+  const [cups, setCups] = useState("");
+
+  const handleContinue = () => {
+    if (cups.length === 5) {
+      setCup(cups);
+    } else {
+      console.error("CUPS incorrecto");
+    }
+  };
+
+  return (
+    <>
+      <FormCups cups={cups} setCups={setCups} />
+      <Link to="/client">
+        <button onClick={handleContinue}>Continuar</button>
+      </Link>
+      <Link to="/#">No tengo el CUPS</Link> // aquí no sé si va a vista a "/client" pero contemplando sin CUPS
+    </>
+  );
 };
 
 export default Cups;
+
