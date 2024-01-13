f08fc33a2acb4edd42122261b4c1a2cd28454239
alejandroFMA <alexfmarquez@gmail.com>
2024-01-13 21:36:39 +0100

    add/context and table energy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,23 +3,24 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import { AuthProvider } from "./context/AuthProvider";
 import { InfoClienteProvider } from "./context/InfoClienteProvider";
-import './App.css'
-
+import { FranjasProvider } from "./context/FranjasProvider";
+import "./App.css";
 
 function App() {
-  
   return (
     <>
       <BrowserRouter>
         <AuthProvider>
-        <InfoClienteProvider>
-            <Header />
-            <Main />
+          <InfoClienteProvider>
+            <FranjasProvider>
+                <Header />
+                <Main />
+            </FranjasProvider>
           </InfoClienteProvider>
         </AuthProvider>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
