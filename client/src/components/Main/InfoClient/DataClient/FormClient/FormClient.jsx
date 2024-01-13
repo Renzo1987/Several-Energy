c7105f84ee50c0e2d40cd68d6229e4786f854257
alejandroFMA <alexfmarquez@gmail.com>
2024-01-13 13:27:48 +0100

    add/infoclienteprovider & change/FormClient

--- client/src/components/Main/InfoClient/DataClient/FormClient/FormClient.jsx
+++ client/src/components/Main/InfoClient/DataClient/FormClient/FormClient.jsx
@@ -1,14 +1,78 @@
-import React from "react";
+import { useState, useContext } from "react";
+import axios from "../../../../../api/axios";
+import { useNavigate } from "react-router-dom";
+import { useInfoCliente } from "../../../../../context/InfoClienteProvider";
 
 const FormClient = () => {
-  return <>
-    <form>   
-      <input type="text" placeholder="Nombre y apellido"/>
-      <input type="text" placeholder="Dirección"/>  
-      <input type="text"  placeholder="Compañía actual"/>
+  const [infoData, setInfoData] = useState({
+    titular: "",
+    direccion: "",
+    comp_actual: "",
+  });
+
+  const { infoClienteState, createInfoCliente } = useInfoCliente();
+
+  const navigate = useNavigate();
+
+  const postInfoCliente = async () => {
+    const infoCliente = {
+      ...infoData,
+      cups: infoClienteState.cup,        
+      usuario_id:"" 
+    };
+
+    try {
+      const response = await axios.post("/api/infocliente", infoCliente);
+      console.log(response.data);
+      navigate("/energy");
+    } catch (error) {
+      console.error("Hubo un error al enviar los datos", error);
+    }
+  };
+
+  const handleChange = (event) => {
+    setInfoData({ ...infoData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    postInfoCliente();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="titular"
+        placeholder="Nombre y apellido"
+        value={infoData.titular}
+        onChange={handleChange}
+        pattern="[a-zA-Z\s]+"
+        title="Solo letras son permitidos."
+      />
+      <input
+        type="text"
+        name="direccion"
+        placeholder="Dirección"
+        value={infoData.direccion}
+        onChange={handleChange}
+        pattern="[a-zA-Z0-9\s]+"
+        title="Solo letras y números son permitidos."
+        required
+      />
+      <input
+        type="text"
+        name="comp_actual"
+        placeholder="Compañía actual"
+        value={infoData.comp_actual}
+        onChange={handleChange}
+        pattern="[a-zA-Z0-9\s]+"
+        title="Solo letras y números son permitidos."
+        required
+      />
+      <button type="submit">Continuar</button>
     </form>
-    <button>Continuar</button>
-  </>;
+  );
 };
 
 export default FormClient;
---
+++ client/src/context/InfoClienteProvider.jsx
@@ -0,0 +1,119 @@
+import React, { createContext, useContext, useReducer } from 'react';
+import axios from "../api/axios";
+
+const InfoClienteContext = createContext();
+
+const SET_CUP = 'SET_CUP';
+const SET_CLIENT_DATA = 'SET_CLIENT_DATA';
+
+const infoClienteReducer = (infoClienteState, action) => {
+  switch (action.type) {
+    case SET_CUP:
+      return {
+        ...infoClienteState,
+        cup: action.payload,
+      };
+    case SET_CLIENT_DATA:
+      return {
+        ...infoClienteState,
+        clientData: action.payload,
+      };
+    default:
+      return infoClienteState;
+  }
+};
+
+const InfoClienteProvider = ({ children }) => {
+  const [infoClienteState, updateInfoClienteState] = useReducer(infoClienteReducer, {
+    cup: '',
+    clientData: {
+      info_id: '',
+      usuario_id: '',
+      direccion: '',
+      comp_actual: '',
+    },
+  });
+
+  const setCup = (cup) => {
+    updateInfoClienteState({ type: SET_CUP, payload: cup });
+  };
+
+  const setClientData = (newClientData) => {
+    updateInfoClienteState({ type: SET_CLIENT_DATA, payload: newClientData });
+  };
+  
+
+  const createInfoCliente = async (usuario_id, titular, direccion, cup, comp_actual) => {
+    try {
+      const response = await axios.post('/infocliente', {
+        usuario_id,
+        titular,
+        direccion,
+        cup,
+        comp_actual,
+      });
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const getInfoCliente = async () => {
+    try {
+      const response = await axios.get('/infocliente/');
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const updateInfoCliente = async (info_id, usuario_id, titular, direccion, cup, comp_actual) => {
+    try {
+      const response = await axios.put(`/infocliente/${info_id}`, {
+        usuario_id,
+        titular,
+        direccion,
+        cup,
+        comp_actual,
+      });
+      return response.data;    
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const deleteInfoCliente = async (info_id) => {
+    try {
+      const response = await axios.delete(`/infocliente/${info_id}`);
+      return response.data;      
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  return (
+    <InfoClienteContext.Provider
+      value={{
+        infoClienteState,
+        setCup,
+        setClientData,
+        createInfoCliente,
+        getInfoCliente,
+        updateInfoCliente,
+        deleteInfoCliente,
+      }}
+    >
+      {children}
+    </InfoClienteContext.Provider>
+  );
+};
+
+const useInfoCliente = () => {
+  const context = useContext(InfoClienteContext);
+  if (!context) {
+    throw new Error("Error");
+  }
+  return context;
+};
+
+export { InfoClienteProvider, useInfoCliente };
