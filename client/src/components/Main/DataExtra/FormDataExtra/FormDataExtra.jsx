a18629682d56b7c427e581880282bafaa3441f31
Mangui <antonio.mangado@gmail.com>
2024-01-14 19:04:01 +0100

    ADD/ styles for extra data

--- client/src/components/Main/DataExtra/FormDataExtra/FormDataExtra.jsx
+++ client/src/components/Main/DataExtra/FormDataExtra/FormDataExtra.jsx
@@ -1,48 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const FormDataExtra = () => {
   return (
-    <>
+    <article id="data-extra-sct">
+      <h5>Datos extra</h5>
       <section className="datos-extra-form">
-        <div className="form-group">
-          <label htmlFor="importeElectrico">Importe eléctrico</label>
-          <input id="importeElectrico" type="text" />
+        <div className="importe-energia">
+          <div className="form-group">
+            <label htmlFor="importeElectrico">Importe eléctrico</label>
+            <input id="importeElectrico" type="text" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="energiaReactiva">Energía reactiva</label>
+            <input id="energiaReactiva" type="text" />
+          </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="energiaReactiva">Energía reactiva</label>
-          <input id="energiaReactiva" type="text" />
+        <div className="importe-energia">
+          <div className="form-group">
+            <label htmlFor="dias-facturación">Días de facturación</label>
+            <input id="dias-facturación" type="text" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="alquiler-equipos">Alquiler de equipos</label>
+            <input id="alquiler-equipos" type="text" />
+          </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="otros1">Otros</label>
-          <input  type="text" placeholder="concepto" />
-          <input  type="text" placeholder="cantidad" />
+        <div className="otros">
+          <label htmlFor="otros">Otros</label>
+          <input  type="text" placeholder="Concepto..." />
+          <input  type="text" placeholder="Cantidad..." />
           <select name="otrosOpcion1">
             <option value="si">Sí</option>
             <option value="no">No</option>
           </select>
-          <select name="otrosOpcion2">
-            <option value="si">Sí</option>
-            <option value="no">No</option>
-          </select>
-          <label htmlFor="otros1">Otros</label>
-          <input  type="text" placeholder="concepto" />
-          <input  type="text" placeholder="cantidad" />
-          <select name="otrosOpcion1">
-            <option value="si">Sí</option>
-            <option value="no">No</option>
-          </select>
-          <select name="otrosOpcion2">
+          <select name="otrosOpcion2" className="last-select">
             <option value="si">Sí</option>
             <option value="no">No</option>
           </select>
         </div>
-        <div className="form-group">
-          <label htmlFor="diasFacturacion">Días de facturación</label>
-          <input id="diasFacturacion" type="text" />
-        </div>
-      </section>
-      <div className="datos-extra-footer">
-        <div className="form-group">
+        <div className="iva">
           <label htmlFor="iva">Iva</label>
           <select name="iva">
             <option value="21">21%</option>
@@ -50,11 +47,16 @@ const FormDataExtra = () => {
             <option value="5">5%</option>
           </select>
         </div>
-        <button>Ver tabla completa</button>
-        <button>Atrás</button>
-        <button>Generar ofertas</button>
-      </div>
-    </>
+      </section>
+      <article className="navigation-sct">
+        <Link to="/client">
+          <button className="back-btn">Atrás</button>
+        </Link>
+        <Link to="/energy">
+          <button  className="continue-btn">Continuar</button>
+        </Link>
+      </article>
+    </article>
   );
 };
 
