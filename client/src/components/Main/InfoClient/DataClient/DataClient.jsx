2c8a63faabc629899323d7c1c9b7f4ce5ce26535
Mangui <antonio.mangado@gmail.com>
2024-01-18 22:36:06 +0100

    ADD/styles

--- client/src/components/Main/InfoClient/DataClient/DataClient.jsx
+++ client/src/components/Main/InfoClient/DataClient/DataClient.jsx
@@ -4,7 +4,7 @@ import FormClient from "./FormClient"
 const DataClient = () => {
   return (
     <section id="client-data-sct">
-      <h1 className="align-start">Introducir datos del cliente</h1>
+      <h4 className="align-start">Introducir datos del cliente</h4>
       <FormClient/>
     </section>
   )
